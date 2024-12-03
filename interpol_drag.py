ee03efe4cddac6e909a6c815456050b0b2a74ed6
Thomas <103991671+TvanZelderen@users.noreply.github.com>
2024-12-03 11:05:03 +0100

    Typo fixed.

--- interpol_drag.py
+++ interpol_drag.py
@@ -4,11 +4,11 @@ def launch_vehicle_drag_coef(mach):
     if mach <= 0.6:
         return 0.2083333 * mach ** 2 - 0.25 * mach + 0.46
     if mach <= 0.8:
-        return 1.25 * mach ** 3 - 2.215 * mach ** 2 + 1.2 * mach + 0.16
+        return 1.25 * mach ** 3 - 2.125 * mach ** 2 + 1.2 * mach + 0.16
     if mach <= 0.95:
         return 10.37037 * mach ** 3 - 22.88889 * mach ** 2 + 16.91111 * mach - 3.78963
     if mach <= 1.05:
-        return - 30 * mach ** 3 + 88.5 * mach ** 2 + 85.425 * mach + 27.51375
+        return - 30 * mach ** 3 + 88.5 * mach ** 2 - 85.425 * mach + 27.51375
     if mach <= 1.15:
         return - 20 * mach ** 3 + 60 * mach ** 2 - 58.65 * mach + 19.245
     if mach <= 1.3:
@@ -23,3 +23,6 @@ def launch_vehicle_drag_coef(mach):
         return 0.22
     else:
         raise ValueError()
+    
+if __name__ == "__main__":
+    print(launch_vehicle_drag_coef(1))
