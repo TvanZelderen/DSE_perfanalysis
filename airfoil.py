6c7d595b994c1b9243370b9753e23d56c4ba09fd
KyanCchenn <K.Chen-11@student.tudelft.nl>
2024-12-04 16:31:34 +0100

    flight profile v3.2

--- airfoil.py
+++ airfoil.py
@@ -20,20 +20,20 @@ wing_folder = 'airfoil_database'   ### Folder directory for Keyan ###
 airfoil_name = 'naca2412'  ## airfoil name, keep it consistent with your file naming ##
 reynolds = '100k'  ## reynolds number for the airfoil, keep it consistent with the data that you downloaded
 
-file_name = airfoil_name + '-' + reynolds + '.csv'
-wing_path = os.path.join(wing_folder, file_name)
 
-with open(wing_path, 'r') as file:
-    data_wing = file.read()
+def f_airfoil(airfoil_name):
+    file_name = airfoil_name + '-' + reynolds + '.csv'
+    wing_path = os.path.join(wing_folder, file_name)
 
-lines = data_wing.strip().split('\n')
-arraycsv = [line.split(',') for line in lines][11:] 
+    with open(wing_path, 'r') as file:
+        data_wing = file.read()
 
-def f_airfoil(airfoil_name, airfoildata = arraycsv):
-    alphaarr = np.array([float(value) for value in np.array(airfoildata)[:, 0]])  ## read the alpha column
-    clarr = np.array([float(value) for value in np.array(airfoildata)[:, 1]])  ## read the cl column
-    cmarr = np.array([float(value) for value in np.array(airfoildata)[:, 4]])  ## read the cm column
-    cdarr = np.array([float(value) for value in np.array(airfoildata)[:, 2]])
+    lines = data_wing.strip().split('\n')
+    arraycsv = [line.split(',') for line in lines][11:] 
+    alphaarr = np.array([float(value) for value in np.array(arraycsv)[:, 0]])  ## read the alpha column
+    clarr = np.array([float(value) for value in np.array(arraycsv)[:, 1]])  ## read the cl column
+    cmarr = np.array([float(value) for value in np.array(arraycsv)[:, 4]])  ## read the cm column
+    cdarr = np.array([float(value) for value in np.array(arraycsv)[:, 2]])
     clalpha_interp = sp.interpolate.interp1d(alphaarr, clarr)
     cmalpha_interp = sp.interpolate.interp1d(alphaarr, cmarr)
     cd0 = float(cdarr[np.where(alphaarr == 0.0)[0][0]])
