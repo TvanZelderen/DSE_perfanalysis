3cbd37be64c7a8390fd1c26835bf5ebabe90b384
Thomas van Zelderen <103991671+TvanZelderen@users.noreply.github.com>
2024-12-06 10:41:07 +0100

    Refactor, max fin torque implemented

--- flight_profile.py
+++ flight_profile.py
@@ -5,6 +5,8 @@ from math import pi
 from plot import plot_flight_states
 from velocity_controller import ImprovedVelocityController
 from airfoil import f_airfoil
+from functools import cache
+from time import perf_counter
 
 def flight_derivatives(state, params):
     """
@@ -52,6 +54,22 @@ def rk4_step(state_0, derivatives_func, params, dt):
 
     return state_1
 
+def max_fin_torque(states, cm_alpha_fin):
+    max_moment = 0
+    alphas = np.arange(-15, 15, 0.25)
+    cms = []
+    for alpha in alphas:
+        try:
+            cms.append(cm_alpha_fin(alpha))
+        except ValueError:
+            pass
+    cma = max(cms)
+    for density, velocity in zip(states['air_density'][::10], states['velocity'][::10]):
+        moment = abs(0.5 * density * velocity**2 * cma * S_fin * (fin_cr + fin_ct) / 2)
+        if moment > max_moment:
+            max_moment = moment
+    return max_moment
+        
 
 dt = 0.01  # time step
 
@@ -111,20 +129,22 @@ states = {
     'cl': [],
 }
 
+manoeuvres = {
+    'landed': False,
+    'turn_delay': 5,
+    'turn': False,
+    'spiral': False,
+    'landing_sequence': False,
+}
+
 velocity_controller = ImprovedVelocityController(
     target_velocity=80,
     min_angle_of_attack=np.deg2rad(-7.5),
     max_angle_of_attack=np.deg2rad(14.0),
 )
 
-landed = False
-turn = True
-spiral = False
 max_bank_angle = np.deg2rad(30)
-useless_distance = 0
-
 landing_angle = np.deg2rad(15)
-landing_sequence = False
 
 
 def initialize_states():
@@ -154,110 +174,124 @@ print("Starting sim...")
 initialize_states()
 print("Initialised states")
 
-while not landed: 
-    current_state = {key: value[-1] for key, value in states.items()}
-    state = (current_state["velocity"], current_state["gamma"])
+start = perf_counter()
 
-    pressure, density, temperature = get_isa(current_state["altitude"])
-    dyn_press = dynamic_pressure(current_state["velocity"], current_state["altitude"])
-    mach = mach_number(current_state["velocity"], current_state["altitude"])
+# @cache
+def run_sim(states):
+    while not manoeuvres['landed']: 
+        current_state = {key: value[-1] for key, value in states.items()}
+        state = (current_state["velocity"], current_state["gamma"])
 
-    alpha = velocity_controller.update(current_state["velocity"], dt)
-    if current_state['altitude'] <= np.tan(landing_angle) * np.sqrt(current_state['x']**2 + current_state['y']**2) and not landing_sequence:
-        landing_sequence = True
-        print("Landing sequence started")
+        pressure, density, temperature = get_isa(current_state["altitude"])
+        dyn_press = dynamic_pressure(current_state["velocity"], current_state["altitude"])
+        mach = mach_number(current_state["velocity"], current_state["altitude"])
 
-    cla_fin = float(clalpha_fin(0))
-    induced_drag_fins = (
-        dyn_press * (cd0_fin + cla_fin**2 / (np.pi * e * Ar_fin)) * S_fin
-    )
-    cla_wing = float(clalpha_wing(alpha))
-    induced_drag_wing = (
-        dyn_press * (cd0_wing + cla_wing**2 / (np.pi * e * Ar_wing)) * S_wing
-    )
-    drag_body = dyn_press * frontal_area * launch_vehicle_drag_coef(mach)
-    drag = induced_drag_fins + induced_drag_wing + drag_body
+        alpha = velocity_controller.update(current_state["velocity"], dt)
+        if current_state['altitude'] <= np.tan(landing_angle) * np.sqrt(current_state['x']**2 + current_state['y']**2) and not manoeuvres['landing_sequence']:
+            manoeuvres['landing_sequence'] = True
+            print("Landing sequence started")
 
-    ########### Turn Implementation ##########
-    if current_state["turn_angle"] >= np.pi and turn: # Initial turn to 0 x distance
-        turn = False
-        print("Backtrack turn completed")
-    if current_state['x'] <= 0 and not spiral and not landing_sequence: # Once at 0 x distance, start the spiral down
-        spiral = True
-        print("Spiral started")
-    if landing_sequence:
-        target_angle = np.arctan2(-current_state['y'], -current_state['x'])
-        alpha = np.deg2rad(14/1.69) # Target velocity = 1.3 V_s
-        spiral = False
+        cla_fin = float(clalpha_fin(0))
+        induced_drag_fins = (
+            dyn_press * (cd0_fin + cla_fin**2 / (np.pi * e * Ar_fin)) * S_fin
+        )
+        cla_wing = float(clalpha_wing(alpha))
+        induced_drag_wing = (
+            dyn_press * (cd0_wing + cla_wing**2 / (np.pi * e * Ar_wing)) * S_wing
+        )
+        drag_body = dyn_press * frontal_area * launch_vehicle_drag_coef(mach)
+        drag = induced_drag_fins + induced_drag_wing + drag_body
 
-    bank_angle = 0
-    horizontal_speed = current_state["velocity"] * np.cos(current_state["gamma"])
-    if turn or spiral:
-        bank_angle = - max_bank_angle
-    elif landing_sequence:
-        if (turn_angle - target_angle) % np.deg2rad(360) < pi: # Turn right
-            bank_angle = max_bank_angle
-        else: # Turn left
+        ########### Turn Implementation ##########
+        if current_state['time'] > manoeuvres['turn_delay'] and current_state['turn_angle'] == 0:
+            manoeuvres['turn'] = True
+        if current_state["turn_angle"] >= np.pi and manoeuvres['turn']: # Initial turn to 0 x distance
+            manoeuvres['turn'] = False
+            print("Backtrack turn completed")
+        if current_state['x'] <= 0 and not manoeuvres['spiral'] and not manoeuvres['landing_sequence']: # Once at 0 x distance, start the spiral down
+            manoeuvres['spiral'] = True
+            print("Spiral started")
+        if manoeuvres['landing_sequence']:
+            target_angle = np.arctan2(-current_state['y'], -current_state['x'])
+            alpha = np.deg2rad(14/1.69) # Target velocity = 1.3 V_s
+            manoeuvres['spiral'] = False
+
+        bank_angle = 0
+        horizontal_speed = current_state["velocity"] * np.cos(current_state["gamma"])
+        if manoeuvres['turn'] or manoeuvres['spiral']:
             bank_angle = - max_bank_angle
-    
-    delta_angle = (
-        current_state["gravity"] * np.tan(bank_angle) / current_state["velocity"]
-    )
-    turn_angle = current_state["turn_angle"] - delta_angle * dt
-    states["turn_angle"].append(turn_angle)
-    horizontal_new = horizontal_speed * np.cos(turn_angle)
-    horizontal_side = horizontal_speed * np.sin(turn_angle)
-    useless_distance += horizontal_side * dt
-    distance = distance = (
-        current_state["distance"] + horizontal_new * dt
-    )  # V * cos(gamma)
-    lift = dyn_press * clalpha_wing(np.rad2deg(alpha)) * S_wing * np.cos(bank_angle)
+        elif manoeuvres['landing_sequence']:
+            if (turn_angle - target_angle) % np.deg2rad(360) < pi: # Turn right
+                bank_angle = max_bank_angle
+            else: # Turn left
+                bank_angle = - max_bank_angle
+        
+        delta_angle = (
+            current_state["gravity"] * np.tan(bank_angle) / current_state["velocity"]
+        )
+        turn_angle = current_state["turn_angle"] - delta_angle * dt
+        states["turn_angle"].append(turn_angle)
+        horizontal_new = horizontal_speed * np.cos(turn_angle)
+        # horizontal_side = horizontal_speed * np.sin(turn_angle)
+        distance = distance = (
+            current_state["distance"] + horizontal_new * dt
+        )
+        lift = dyn_press * clalpha_wing(np.rad2deg(alpha)) * S_wing * np.cos(bank_angle)
 
-    load_factor = dyn_press * clalpha_wing(np.rad2deg(alpha)) * S_wing / current_state['weight']
+        load_factor = dyn_press * clalpha_wing(np.rad2deg(alpha)) * S_wing / current_state['weight']
 
-    dx = current_state['velocity'] * np.cos(current_state['turn_angle']) * dt
-    dy = current_state['velocity'] * np.sin(current_state['turn_angle']) * dt
-    x = current_state['x'] + dx
-    y = current_state['y'] + dy
+        dx = current_state['velocity'] * np.cos(state[1]) * np.cos(current_state['turn_angle']) * dt
+        dy = current_state['velocity'] * np.cos(state[1]) * np.sin(current_state['turn_angle']) * dt
+        x = current_state['x'] + dx
+        y = current_state['y'] + dy
 
-    states["lift"].append(lift)
-    states["drag"].append(drag)
+        states["lift"].append(lift)
+        states["drag"].append(drag)
 
-    current_state["lift"] = states["lift"][-1]
-    current_state["drag"] = states["drag"][-1]
+        current_state["lift"] = states["lift"][-1]
+        current_state["drag"] = states["drag"][-1]
 
-    # Integrate velocity and gamma
-    state = rk4_step(state, flight_derivatives, current_state, dt)
+        # Integrate velocity and gamma
+        state = rk4_step(state, flight_derivatives, current_state, dt)
 
-    vertical_speed = state[0] * np.sin(state[1])
-    altitude = current_state["altitude"] + vertical_speed * dt
+        vertical_speed = state[0] * np.sin(state[1])
+        altitude = current_state["altitude"] + vertical_speed * dt
 
-    time = current_state["time"] + dt
+        time = current_state["time"] + dt
 
-    # Append states
-    states["time"].append(time)
-    states["velocity"].append(state[0])
-    states["gamma"].append(state[1])
-    states["altitude"].append(altitude)
-    states["mach"].append(mach)
-    states["alpha"].append(alpha)
-    states["vertical_speed"].append(vertical_speed)
-    states["distance"].append(distance)
-    states["load_factor"].append(load_factor)
-    states["x"].append(x)
-    states["y"].append(y)
+        # Append states
+        states["time"].append(time)
+        states["velocity"].append(state[0])
+        states["gamma"].append(state[1])
+        states["altitude"].append(altitude)
+        states["mach"].append(mach)
+        states["alpha"].append(alpha)
+        states["vertical_speed"].append(vertical_speed)
+        states["distance"].append(distance)
+        states["load_factor"].append(load_factor)
+        states["x"].append(x)
+        states["y"].append(y)
 
-    states["air_density"].append(density)
-    states["cl"].append(clalpha_wing(np.rad2deg(alpha)))
+        states["air_density"].append(density)
+        states["cl"].append(clalpha_wing(np.rad2deg(alpha)))
 
-    if current_state["altitude"] < 0:
-        landed = True
+        if current_state["altitude"] < 0:
+            manoeuvres['landed'] = True
+
+    return states
+
+run_sim(states)
+
+run_time = perf_counter() - start
+print(f"Runtime of simulation: {run_time:.2f}")
 
 print(f"Flight duration: {round(states["time"][-1],1)}")
-print(f"Vertical speed at touchdown: {current_state['vertical_speed']}")
-print(f"Position at touchdown: {round(current_state['x']), round(current_state['y'])}")
+print(f"Vertical speed at touchdown: {states['vertical_speed'][-1]}")
+print(f"Position at touchdown: {round(states['x'][-1]), round(states['y'][-1])}")
 
 plot_flight_states(states)
+
+print(f"Max fin torque: {max_fin_torque(states, cmalpha_fin)}")
 
 export = False
 if export:
