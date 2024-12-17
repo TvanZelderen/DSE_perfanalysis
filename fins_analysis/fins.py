77d472e30d3d2a24d84d3eeefbb90c72eb72260f
domispecht <dominika.specht@gmail.com>
2024-12-17 15:45:37 +0100

    init commit

--- fins_analysis/fins.py
+++ fins_analysis/fins.py
@@ -11,11 +11,27 @@ from utils import get_isa, get_local_speed_of_sound, mach_number
 #assume the fuselage is a blunt body
 
 
+
 #initialise values for stagnation pressure calc
 p_stag = 0          #stagnation pressure
-p_static = 0        #static pressure
+p_static = 1.9725*10**(3)       #static pressure
 gamma = 1.404       #specific heat of air at -15C (because i didnt find -50C)
-M = 0               #Mach number
+M = 0.738   #Mach number
+S = 0.1**2 #surface area
+rho = 3.1076*10**(-2)   #density
+V = 220 #velocity
 
 p_stag = p_static *( (1 + ((gamma - 1)/2)*M**2) ** (gamma/(gamma -1)))      #https://en.wikipedia.org/wiki/Stagnation_pressure
+
+D = p_stag*S
+
+C_d = D/(0.5*rho*V**2*S)
+
+print(C_d/2)
+
+C_dcalc = 2.714*10**3*S/(0.5*0.03102*V**2*S) #based on NASA numbers
+
+print(C_dcalc/2)
+
+
 
