6c6035ed0550d6841349db642508e7b0da188ebe
KyanCchenn <K.Chen-11@student.tudelft.nl>
2024-12-19 16:20:07 +0100

    minor update

--- simulation/airfoil.py
+++ simulation/airfoil.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 wing_folder = 'airfoil_database'   ### Folder directory for Keyan ###
 
-airfoil_name = 'kc135'  ## airfoil name, keep it consistent with your file naming ##
+airfoil_name = 'kc135'  ## airfoil name ##
 Re1 = '30k'
 Re2 = '50k'
 Re3 = '100k'
@@ -27,32 +27,18 @@ Re4 = '250k'
 Re5 = '500k'
 Re6 = '800k'
 
-
-
-# airfoil_files = {airfoil_30k,
-#            airfoil_50k,
-#            airfoil_100k, 
-#            airfoil_250k, 
-#            airfoil_500k, 
-#            airfoil_800k}
-
-
-# xflr5_data = pd.DataFrame(alpha, columns = ['alpha'])
-# print(xflr5_data)
-
 def f_airfoil(airfoil_name):
+
     alphaarr = np.arange(-10, 15, 0.25)
     clinterp = np.empty((0, len(alphaarr)))
     cdinterp = np.empty((0, len(alphaarr)))
     cminterp = np.empty((0, len(alphaarr)))
     Reyarr = np.array([30000, 50000, 100000, 250000, 500000, 800000])
-    ultimate_solution1 = np.array([])
-    ultimate_solution2 = np.array([])
-    ultimate_solution3 = np.array([])
-
-
+    cl_interpolation = np.array([])
+    cd_interpolation = np.array([])
+    cm_interpolation = np.array([])
     
-    print(np.shape(alphaarr))
+    # print(np.shape(alphaarr))
 
     airfoils = [
     airfoil_name + '-' + Re1 + '.csv',
@@ -66,11 +52,11 @@ def f_airfoil(airfoil_name):
     for i in range(0, len(airfoils)):  
         data_wing = pd.read_csv(os.path.join(Path('airfoil_database') , airfoils[i]))
         alpha = np.array(data_wing.iloc[:, 0])
+
         cl = np.array(data_wing.iloc[:, 1])
         clalpha = sp.interpolate.interp1d(alpha, cl)
         clinterp = np.vstack((clinterp, clalpha(alphaarr)))
 
-        # print(clinterp)
         cd = np.array(data_wing.iloc[:, 2])
         cdalpha = sp.interpolate.interp1d(alpha, cd)
         cdinterp = np.vstack((cdinterp, cdalpha(alphaarr)))
@@ -78,25 +64,23 @@ def f_airfoil(airfoil_name):
         cm = np.array(data_wing.iloc[:, 4])
         cmalpha = sp.interpolate.interp1d(alpha, cm)
         cminterp = np.vstack((cminterp, cmalpha(alphaarr)))
-        # print(cl)
 
 
     for i in range(0, len(alphaarr)):
         clreynolds = sp.interpolate.interp1d(Reyarr, clinterp[:, i])
         cdreynolds = sp.interpolate.interp1d(Reyarr, cdinterp[:, i])
-        cmreynolds = sp.interpolate.interp1d(Reyarr, cdinterp[:, i])
-        ultimate_solution1 = np.append(ultimate_solution1, clreynolds)
-        ultimate_solution2 = np.append(ultimate_solution2, cdreynolds)
-        ultimate_solution3 = np.append(ultimate_solution3, cmreynolds)
-        # cd = data_wing['CD'].to_list()
-        # ultimate_solution = np.vstack((ultimate_solution1, ultimate_solution2, ultimate_solution3))
+        cmreynolds = sp.interpolate.interp1d(Reyarr, cminterp[:, i])
 
-            # xflr5_data.
-    # cm = cmarr[np.where(alphaarr == alpha)]
-    # print(cd0)
-    return ultimate_solution1, ultimate_solution2, ultimate_solution3
+        cl_interpolation = np.append(cl_interpolation, clreynolds)
+        cd_interpolation = np.append(cd_interpolation, cdreynolds)
+        cm_interpolation = np.append(cm_interpolation, cmreynolds)
+
+    return cl_interpolation, cd_interpolation, cm_interpolation
 
 if __name__ == '__main__':
     airfoil_wing = 'kc135'
     clinterp, cdinterp, cminterp = f_airfoil(airfoil_name = airfoil_wing)
-    print(clinterp[40](30000))
+    print(clinterp[72](700000))
+
+
+
---
+++ simulation/presets.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+############  Launch Ring Characteristics  ##########
+m = 20
+L_body = 0.8
+L_nose = 1
+
+############  Wing Characteristics  ###########
+winglength = 1
+
+wingchord_root = 0.13
+wingchord_tip = 0.13
+sweep = 0   # degrees
+effective_span = np.cos(np.deg2rad(sweep)) * winglength
+S_wing = (wingchord_root + wingchord_tip) * effective_span / 2
+Ar_wing = effective_span ** 2 / S_wing
+e = 0.65
+
+############  Fin Characteristics  ###########
+finspan = 0.1
+finchord_root = 0.1
+finchord_tip = 0.05
+
+S_fin = (finchord_root + finchord_tip) * finspan / 2
+Ar_fin = finspan ** 2 / S_fin
+
+############  Physics  ###########
+G = 9.80665
+
+############  Starting Conditions  ###########
+altitude = 27000
+distance = 16000
+velocity = 200
+
+
+
+
+
+
+
+
+
